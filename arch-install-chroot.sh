c3f202339120ca7ef50a2a844503783b5830b989
Jebusthebus <jebus@net-c.com>
2023-11-15 04:04:01 +0100

    Fixed some more typos

--- arch-install-chroot.sh
+++ arch-install-chroot.sh
@@ -21,4 +21,4 @@ passwd stefan98
 
 grub-install --target=x86_64-efi --efi-directory=/boot/ --bootloader-id=GRUB
 
-grub-mkconfig -i /boot/grub/grub.cfg
+grub-mkconfig -o /boot/grub/grub.cfg
--- arch-install.sh
+++ arch-install.sh
@@ -6,13 +6,13 @@ reflector --latest 5 --protocol https --sort rate --country Germany,France --sav
 
 # Install arch and some useful programs
 
-pacstrap -K /mnt base linux-zen linux-zen-headers linux-firmware base-devel networkmanager vim nvim man-db man-pages texinfo grub efibootmgr amd-ucode reflector git
+pacstrap -K /mnt base linux-zen linux-zen-headers linux-firmware base-devel networkmanager vim neovim man-db man-pages texinfo grub efibootmgr amd-ucode reflector git
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Run the chroot part of the script
 chmod +x arch-install-chroot.sh
-mv arch-install-chroot.sh /mnt
-arch-chroot /mnt /bin/bash arch-install-chroot.sh
+cp arch-install-chroot.sh /mnt || exit
+arch-chroot /mnt ./arch-install-chroot.sh
 
 reboot
